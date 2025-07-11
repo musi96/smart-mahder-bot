6510e39f2fd31f5e29d7069780f971163244bf0b
musi96 <museabmohammed23@gmail.com>
2025-07-11 15:29:07 +0300

    build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 set -o errexit
 
+# Install system dependencies for Pillow
+apt-get update
+apt-get install -y \
+    libjpeg-dev \
+    libtiff5-dev \
+    libfreetype6-dev \
+    liblcms2-dev \
+    libwebp-dev \
+    zlib1g-dev
+
+# Install Python packages
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
